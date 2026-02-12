a6e4d41eeb11501211a3608d9da2c7207599ae1d
Alief Muhammad Abdillah <aliefmabdillah09@gmail.com>
2026-02-12 11:21:02 +0700

    refactor: api url

--- src/api/config.tsx
+++ src/api/config.tsx
@@ -1,4 +1,4 @@
-// const API_URL = 'https://api-dashboard.getsensync.com/api/sparing';
-const API_URL = 'http://localhost:3000/api/sparing';
+const API_URL = 'https://api-dashboard.getsensync.com/api/sparing';
+// const API_URL = 'http://localhost:3000/api/sparing';
 
 export { API_URL };
